191135908bcfe7a880fe108999cfd23fa68c5796
Fabien Jeckelmann <fabien.jeckelmann@gmail.com>
2019-11-26 16:11:03 +0100

    responsive canvas

---
+++ src/components/Canvas.vue
@@ -0,0 +1,29 @@
+<template>
+<canvas :width="width" :height="height"></canvas>
+</template>
+
+<script>
+export default {
+  name: 'Canvas',
+  data: () => ({
+    width: Number,
+    height: Number,
+  }),
+  mounted() {
+    window.addEventListener('resize', this.handleResize);
+    this.handleResize();
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.handleResize);
+  },
+  methods: {
+    handleResize() {
+      this.width = window.innerWidth;
+      this.height = window.innerHeight;
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+</style>
